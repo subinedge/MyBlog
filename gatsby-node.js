8f803c053b77fc63a325fe9fb592195cb7855ca9
Subin <subinsamrat96@gmail.com>
2019-11-17 21:00:16 +0530

    Revert "sample2"
    This reverts commit b4336c9a7b33f316e55d6f4e10f1ddec24fab683.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,11 @@
-const path = require("path")
+const path = require('path')
 
-// onCreateNode used to generate slug for posts which we accessed through graphql API, but now no need because we added a slug field in contentful
+
+ // onCreateNode used to generate slug for posts which we accessed through graphql API, but now no need because we added a slug field in contentful
+
 
 // module.exports.onCreateNode = ({node, actions}) => {
-
+ 
 //   const {createNodeField} = actions
 
 //   if(node.internal.type === "MarkdownRemark") {
@@ -16,33 +18,35 @@ const path = require("path")
 //       value: slug
 //     })
 //   }
-
+  
 // }
 
-module.exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
 
-  const blogTemplate = path.resolve("./src/templates/blog.js")
+
+module.exports.createPages = async ({graphql, actions}) => {
+  const {createPage} = actions
+
+  const blogTemplate = path.resolve('./src/templates/blog.js')
 
   const res = await graphql(`
-    query {
-      allContentfulBlogPost {
-        edges {
-          node {
-            slug
-          }
+  query {
+    allContentfulBlogPost {
+      edges {
+        node {
+          slug
         }
       }
     }
+  }
   `)
 
-  res.data.allContentfulBlogPost.edges.forEach(edge => {
-    createPage({
+  res.data.allContentfulBlogPost.edges.forEach((edge)=> {
+    createPage ({
       component: blogTemplate,
-      path: `/blog/${edge.node.slug}`,
+      path: `/blog/${edge.node.slug}`, 
       context: {
-        slug: edge.node.slug,
-      },
+        slug: edge.node.slug
+      }
     })
   })
 
@@ -50,4 +54,5 @@ module.exports.createPages = async ({ graphql, actions }) => {
   // 1. get path to template
   // 2. get markdown data
   // 3. create new pages
+  
 }
--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,8 +1,8 @@
-import React from "react"
-import { graphql } from "gatsby"
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
-import Layout from "../components/layout"
-import Head from "../components/head"
+import React from 'react'
+import {graphql} from 'gatsby'
+import {documentToReactComponents} from '@contentful/rich-text-react-renderer'
+import Layout from '../components/layout'
+import Head from '../components/head'
 
 // export const query = graphql`
 // query (
@@ -24,9 +24,9 @@ import Head from "../components/head"
 // }
 // `
 
-export const query = graphql`
+export const query = graphql `
   query($slug: String!) {
-    contentfulBlogPost(slug: { eq:$slug }) {
+    contentfulBlogPost(slug: {eq:$slug}) {
       title
       publishedDate(formatString: "MMMM Do, YYYY")
       body {
@@ -36,20 +36,24 @@ export const query = graphql`
   }
 `
 
-const Blog = props => {
+  
+
+
+const Blog = (props)=> {
+
   const options = {
     renderNode: {
-      "embedded-asset-block": node => {
-        const alt = node.data.target.fields.title["en-US"];
-        const url = node.data.target.fields.file["en-US"].url;
-        return <img alt={alt} src={url} />
-      },
-    },
+      "embedded-asset-block" : (node) => {
+        const alt = node.data.target.fields.title['en-US'];
+        const url = node.data.target.fields.file['en-US'].url;
+        return <img alt={alt} src={url}/>
+      }
+    }
   }
 
-  return (
+  return(
     <Layout>
-      <Head title={props.data.contentfulBlogPost.title} />
+      <Head title={props.data.contentfulBlogPost.title}/>
       {/* <h1>{props.data.markdownRemark.frontmatter.title}</h1>
       <p>{props.data.markdownRemark.frontmatter.date}</p>
 
@@ -61,6 +65,6 @@ const Blog = props => {
       {documentToReactComponents(props.data.contentfulBlogPost.body.json, options)}
     </Layout>
   )
-}
+} 
 
-export default Blog
+export default Blog;
